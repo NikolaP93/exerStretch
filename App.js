cfc8c20d0a74907b8c849ab9eedb1af27ee143f6
Nikola Prijovic <nikola.prijovic1@gmail.com>
2020-02-07 00:19:26 +0100

    added linting

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-// createStackNavigator is a function that takes a route configuration object and an options object and returns a React component.
+// function that takes a route configuration object and an options object and returns a component.
 
 import Landing from './components/pages/Landing';
 import Account from './components/pages/Account';
@@ -9,32 +9,32 @@ import Welcome from './components/pages/Welcome';
 
 const App = createStackNavigator(
 
-  //shorthand config
+  // shorthand config
   {
     Landing: {
       screen: Landing,
       navigationOptions: {
-        header: null
-      }
+        header: null,
+      },
     },
     Account: {
       screen: Account,
       navigationOptions: {
-        headerTitle: 'Account'
-      }
+        headerTitle: 'Account',
+      },
     },
     Welcome: {
       screen: Welcome,
       navigationOptions: {
-        header: null
-      }
-    }
+        header: null,
+      },
+    },
   },
   {
-    initialRouteName: 'Landing'
-  }
+    initialRouteName: 'Landing',
+  },
 );
 
 
 // navigation container
-export default createAppContainer(App)
+export default createAppContainer(App);
--- components/Card.js
+++ components/Card.js
@@ -1,44 +1,43 @@
 import React from 'react';
 import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 
-const Card = props => {
-
-    const { navigate } = props.navigation;
-
-    // if a function is passed to the card, execute it as well
-    const navigateAndExecuteFunction = () => {
-        if (props.signin) {
-            props.signin();
-        } else {
-            navigate(props.href)
-        }
-    }
-
-    return (
-        // inherits parents styles plus it's own, on click changes to given prop
-        <TouchableOpacity
-            style={{ ...styles.container, ...props.style }}
-            activeOpacity={.5}
-            onPress={() => { navigateAndExecuteFunction() }}>
-            <Text style={{ ...styles.text }}>{props.title}</Text>
-        </TouchableOpacity>
-    )
-}
-
 const styles = StyleSheet.create({
-    container: {
-        width: 350,
-        height: 60,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 50,
-        backgroundColor: '#0db5ba'
-    },
-    text: {
-        textTransform: 'uppercase',
-        fontWeight: 'bold',
-        fontSize: 20,
+  container: {
+    width: 350,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+    backgroundColor: '#0db5ba',
+  },
+  text: {
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
+
+const Card = (props) => {
+  // if a function is passed to the card, execute it as well
+  const navigateAndExecuteFunction = () => {
+    if (props.signin) {
+      props.signin();
+    } else {
+      props.navigation.navigate(props.href);
     }
-})
+  };
+
+  return (
+  // inherits parents styles plus it's own, on click changes to given prop
+    <TouchableOpacity
+      style={{ ...styles.container, ...props.style }}
+      activeOpacity={0.5}
+      onPress={() => { navigateAndExecuteFunction(); }}
+    >
+      <Text style={styles.text}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+};
+
 
 export default Card;
